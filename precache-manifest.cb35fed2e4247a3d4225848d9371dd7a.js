a4751aa70a2f140e27e2e8c752d093134cb58ef4
JKGhartey <79650095+JKGhartey@users.noreply.github.com>
2021-11-08 12:20:30 +0000

    Updates

---
+++ precache-manifest.cb35fed2e4247a3d4225848d9371dd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d3c11be37f8912c54ed",
+    "url": "/JKGhartey/podex.git/static/css/main.7e078a81.chunk.css"
+  },
+  {
+    "revision": "6d3c11be37f8912c54ed",
+    "url": "/JKGhartey/podex.git/static/js/main.6d3c11be.chunk.js"
+  },
+  {
+    "revision": "8fc3d7552dd9972cf92c",
+    "url": "/JKGhartey/podex.git/static/css/1.7342c58d.chunk.css"
+  },
+  {
+    "revision": "8fc3d7552dd9972cf92c",
+    "url": "/JKGhartey/podex.git/static/js/1.8fc3d755.chunk.js"
+  },
+  {
+    "revision": "80c913229b3e6a9093c7",
+    "url": "/JKGhartey/podex.git/static/js/runtime~main.80c91322.js"
+  },
+  {
+    "revision": "dfaea88e77175b61f893a073ffdb49a3",
+    "url": "/JKGhartey/podex.git/index.html"
+  }
+];
